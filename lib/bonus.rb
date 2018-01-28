d214dfb77bd991d30ed316bde1bab2605f8d76c6
Brennan Heisler <learn@heislercreative.com>
2018-01-28 04:06:34 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
   #code your solution here:
-
-  
+def bonus
+  epic_tragedy[:Montague][:hero][:status] = ""
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
